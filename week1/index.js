5544e90cad076984c9027457d4e433d422bd93b6
Sean Cheng <chengls94204@gmail.com>
2022-08-25 04:13:41 +0800

    Week 2: + figcaption + change src after submit

--- week1/index.js
+++ week1/index.js
@@ -1,3 +1,22 @@
 function cleartext() {
   document.getElementById("Textarea").value = "";
 }
+
+function submitAlert() {
+  alert("submitted");
+}
+
+function avoidJump () {
+  return false ;
+}
+
+
+function changeSrc() {
+  document.getElementById("image1").src="https://1.bp.blogspot.com/-tVeC6En4e_E/X96mhDTzJNI/AAAAAAABdBo/jlD_jvZvMuk3qUcNjA_XORrA4w3lhPkdQCNcBGAsYHQ/s1048/onepiece01_luffy.png" ;
+  submitAlert();
+  event.preventDefault();
+}
+
+
+//https://1.bp.blogspot.com/-tVeC6En4e_E/X96mhDTzJNI/AAAAAAABdBo/jlD_jvZvMuk3qUcNjA_XORrA4w3lhPkdQCNcBGAsYHQ/s1048/onepiece01_luffy.png
+//"https://ppt.cc/fzRhax@.png"
