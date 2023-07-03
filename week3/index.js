057390a043a9a160af0ffb938ba1e4dd602ee162
Long Sing Cheng <cheng942042@gmail.com>
2023-07-03 23:01:08 +0800

    update: 1. fixed changeSrc()              2. Switch case edited.

--- week3/index.js
+++ week3/index.js
@@ -11,36 +11,56 @@ function avoidJump () {
 }
 
 function vaildate1(event) {
+  var s1 = document.getElementById("male");
+  var s2 = document.getElementById("female");
+  var s3 = document.getElementById("cfu");
   var c1 = document.getElementById("habit1");
   var c2 = document.getElementById("habit2");
   var c3 = document.getElementById("habit3");
-  // if (c1.checked === true){
-  //   console.log(c1.value);
-  // }
-  // if (c2.checked === true){
-  //   console.log(c2.value);
-  // } else {
-  //   return null;
-  // }
-  // if (c3.checked === true){
-  //   console.log(c3.value);
-  // } else {
-  //   return null;
-  // }
-  
 
-  switch (true){
-    case (c1.checked === true):
-      console.log(c1.value);
-       break;
-    case (c2.checked === true):
-      break;
-      console.log(c2.value);
-    case (c3.checked === true):
-      console.log(c3.value);
-      break;
-      default:
+if (s2.checked){
+  if(c1.checked){
+    switch(true){
+      case (c2.checked && c3.checked):
+        alert("Female can't fap, diuhi and callFBI.")
         break;
+      case (c3.checked):
+        alert("Female can't callFBI.")
+        break;
+    }
+
+  }
+}
+
+
+
+
+
+    switch (true){
+      // case ( c1.checked && c2.checked && c3.checked === true ):
+      //   alert("Female can't fap, diuhi and callFBI.")
+      //   break;
+      // case (c1.checked && c2.checked === true ):
+      //   alert("Female can't fap and diuhi.")
+      //   break;
+      // case ( c1.checked && c3.checked === true ):
+      //   alert("Female can't fap and callFBI.")
+      //   break;
+      // case ( c2.checked && c3.checked === true ):
+      //     alert("Female can't diuhi and callFBI.")
+      //     break;
+      // case ( c1.checked === true ):
+      //   alert("Female can't fap.")
+      //    break;
+      // case ( c2.checked === true ):
+      //   alert("Female can't diuhi.")
+      //   break;
+      // case ( c3.checked === true ):
+      //   alert("Female can't callFBI.")
+      //   break;
+        default:
+          break;
+    }
   }
 
 }
@@ -53,10 +73,10 @@ function getsubmitdata (event) {
   return false;
 }
 
-function changeSrc(event) {
+function changeSrc() {
   try {
   event.preventDefault();
-  document.getElementById("image1").src="https://1.bp.blogspot.com/-tVeC6En4e_E/X96mhDTzJNI/AAAAAAABdBo/jlD_jvZvMuk3qUcNjA_XORrA4w3lhPkdQCNcBGAsYHQ/s1048/onepiece01_luffy.png" ;
+  document.getElementById("image1").src = "https://1.bp.blogspot.com/-tVeC6En4e_E/X96mhDTzJNI/AAAAAAABdBo/jlD_jvZvMuk3qUcNjA_XORrA4w3lhPkdQCNcBGAsYHQ/s1048/onepiece01_luffy.png" ;
   getsubmitdata(event);
   // submitAlert();
   } catch(err) {
@@ -66,6 +86,10 @@ function changeSrc(event) {
 
 function demo1 (){
   document.getElementById("male").innerHTML = p
+}
+
+function changeAudio(){
+  document.getElementById()
 }
 
 //https://1.bp.blogspot.com/-tVeC6En4e_E/X96mhDTzJNI/AAAAAAABdBo/jlD_jvZvMuk3qUcNjA_XORrA4w3lhPkdQCNcBGAsYHQ/s1048/onepiece01_luffy.png
